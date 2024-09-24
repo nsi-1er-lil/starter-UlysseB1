12a4963302ec080c1ec78d3085b9726e5af1aee6
UlysseB1 <ulysseb@lyceechurchill.london>
2024-09-24 14:17:38 +0000

    exercice4

---
+++ Debuter avec Python/exercice_4.py
@@ -0,0 +1,26 @@
+print('----------------------------------------------------')
+print('Question 6') # question 6
+cote_du_carre = 2
+c = cote_du_carre
+p = 4 * c 
+a = c**2
+b = (a>5)
+print('Le cote du carre est : ',c)
+print('Le perimetre du carre est : ',p )
+print('La surface du carre est : ', a)
+print(b)
+
+# separation des codes 
+print('------------------------------------------------------')
+
+print('Question 7') #question 7
+def perimetre(x):
+    return x*4
+print('le perimetre est :',perimetre(5))
+
+print('------------------------------------------------------')
+
+print('Question 8')# question 8
+def surface(x):
+    return x*x
+print('la surface du carre est :',surface(10))
