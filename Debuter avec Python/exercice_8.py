c353a462a9c92260bfae2bb9412163f60f0eaae4
UlysseB1 <ulysseb@lyceechurchill.london>
2024-09-26 23:01:07 +0000

    exercice8

---
+++ Debuter avec Python/exercice_8.py
@@ -0,0 +1,6 @@
+def Vitesse_moyenne():
+    return d/t 
+d = 1200
+t = 15
+print("La vitesse moyenne sur une distance de",d,"metres, avec un"
+      " temps de",t,"secondes, est de :", Vitesse_moyenne(),'m/s')
